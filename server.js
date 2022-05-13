2417c0e40a52dded9a26af76a664c0b223e5010d
Tom <trgadacy@quinnipiac.edu>
2022-05-13 00:35:46 -0400

    fix: bug in server.js

--- server.js
+++ server.js
@@ -11,10 +11,12 @@ const path = require("path");
 
 const app = express();
 
+
 // Middleware
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
+app.use(express.static("public"))
 app.use(session({
   secret: process.env.SECRET,
   resave: true,
@@ -48,11 +50,11 @@ db.on("error", console.error.bind(console, "Connection error: "));
 // Routes
 app.use("/user", userRouter);
 
-app.get("/*", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "index.html"), (err) => {
-    res.status(500).send(err);
-  });
-});
+// app.get("/*", (req, res) => {
+//   res.sendFile(path.join(__dirname, "public", "index.html"), (err) => {
+//     res.status(500).send(err);
+//   });
+// });
 
 app.listen(process.env.PORT, () => {
   console.log(`Server started on port ${process.env.PORT}`);
